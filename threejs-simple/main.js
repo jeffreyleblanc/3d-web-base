9bd968f8a6cda6e434385f90cff83bf7ef84712d
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2024-02-06 14:58:43 -0500

    Update to the threejs stuff.

--- threejs-simple/main.js
+++ threejs-simple/main.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+
+// import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //-- Core Components -----------------------------------------//
 
@@ -26,10 +31,21 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
     const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.25 );
     scene.add( light );
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5, {
-        position: new THREE.Vector3(15,0,0)
-    } );
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
+    directionalLight.position.set(15,15,0);
     scene.add( directionalLight );
+
+    const helper = new THREE.DirectionalLightHelper( directionalLight, 1 );
+    scene.add( helper );
+
+    // Note this doesn't seem to reflect off the lambert mesh
+    const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
+    pointLight.position.set( 10, 10, 10 );
+    scene.add( pointLight );
+
+    const sphereSize = 1;
+    const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+    scene.add( pointLightHelper );
 
 //-- Helpers -----------------------------------------//
 
@@ -48,6 +64,75 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
     const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
+    cube.position.set(2,2,0);
+
+    const geometry2 = new THREE.BoxGeometry( 1, 2, 3 );
+    const material2 = new THREE.MeshLambertMaterial( { color: 0xffff00 } );
+    const cube2 = new THREE.Mesh( geometry2, material2 );
+    scene.add( cube2 );
+    cube2.position.set(10,5,3);
+
+//-- Lines ---------------------------------------------//
+
+    //create a blue LineBasicMaterial
+    const line_material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+    const line_geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+    const line = new THREE.Line( line_geometry, line_material );
+
+    scene.add( line );
+
+//-- Text ---------------------------------------------//
+
+    // https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_text.html
+
+    // https://threejs.org/docs/#manual/en/introduction/Creating-text
+
+    // var content = '<div>' +
+    //   '<h1>This is an H1 Element.</h1>' +
+    //   '<span class="large">Hello Three.js cookbook</span>' +
+    //   '<textarea> And this is a textarea</textarea>' +
+    // '</div>';
+
+//     // const loader = new FontLoader();
+// const loader = new FontLoader();
+
+
+// loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+//     const geometry = new TextGeometry( 'Hello three.js!', {
+//         font: font,
+//         size: 80,
+//         height: 5,
+//         curveSegments: 12,
+//         bevelEnabled: true,
+//         bevelThickness: 10,
+//         bevelSize: 8,
+//         bevelOffset: 0,
+//         bevelSegments: 5
+//     } );
+// } );
+
+    const model_loader = new GLTFLoader();
+
+    model_loader.load( 'public/model1.glb', function ( gltf ) {
+        console.log("ADDED!")
+        scene.add( gltf.scene );
+
+        window.MODEL = gltf.scene;
+        gltf.scene.scale.set(10,10,10)
+
+    }, undefined, function ( error ) {
+
+        console.error( error );
+
+    } );
 
 //-- Controls -----------------------------------------//
 
@@ -60,8 +145,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 //-- Loop -----------------------------------------//
 
     function animate() {
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
 
         requestAnimationFrame( animate );
 
